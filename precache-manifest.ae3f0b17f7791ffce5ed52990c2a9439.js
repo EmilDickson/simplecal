99261dfc00b4233a234e6042753eaebb015b589a
Emil Dickson <emil.dickson@gmail.com>
2019-03-12 14:30:30 +0100

    Updates

---
+++ precache-manifest.ae3f0b17f7791ffce5ed52990c2a9439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32898cb1da0372fdae10",
+    "url": "/simplecal/static/js/runtime~main.32898cb1.js"
+  },
+  {
+    "revision": "7da7ec537f73ae87179e",
+    "url": "/simplecal/static/js/main.7da7ec53.chunk.js"
+  },
+  {
+    "revision": "5ffc63a311d806fa469f",
+    "url": "/simplecal/static/js/2.5ffc63a3.chunk.js"
+  },
+  {
+    "revision": "7da7ec537f73ae87179e",
+    "url": "/simplecal/static/css/main.09282b12.chunk.css"
+  },
+  {
+    "revision": "43e7ef8ac39c31ac8158e42c51e92fde",
+    "url": "/simplecal/index.html"
+  }
+];
